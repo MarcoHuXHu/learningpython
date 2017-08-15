3eb936ed92f0613649bae3a4a132a9ba65c183ea
MarcoHuXHu <huxhu2333@gmail.com>
2017-08-15 17:43:26 +0800

    understanding fork

--- multiprocessing/fork.py
+++ multiprocessing/fork.py
@@ -2,18 +2,22 @@ import os
 
 print('Process (%s) start...' % os.getpid())
 
-pid = os.fork()
+def run1():
+    pid = os.fork()
 
-if pid == 0 :
-	print('Child process, %s is son of %s' % (os.getpid(), os.getppid()))
-else :
-	print('%s created %s' % (os.getpid(), pid))
-
-pid = os.fork()
-
-if pid == 0 :
+    if pid == 0 :
         print('Child process, %s is son of %s' % (os.getpid(), os.getppid()))
-else :
+    else :
         print('%s created %s' % (os.getpid(), pid))
 
 
+run1() # 分为两个进程执行
+# 0
+# |
+# 1
+run1() # 两个进程都会执行这一句
+# 0
+# |\
+# 1 2
+# |
+# 3
