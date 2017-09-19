b0d3fd510d1004da30b7157db47d71383c7a8719
MarcoHuXHu <huxhu2333@gmail.com>
2017-09-19 23:30:39 +0800

    list generator

--- BasicPython/use-list.py
+++ BasicPython/use-list.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     print(init(3,4))
 
 
-# slice
+# Slice
 L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
 print('L[0:3] =', L[0:3])
 print('L[:3] =', L[:3])
@@ -40,3 +40,14 @@ R[10:20] = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 R[:10:2] = [0, 2, 4, 6, 8]
 R[::5] = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
 '''
+
+# List Generator
+print(list(range(1, 11))) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 注意是10不是11
+print([x * x for x in range(1, 11)]) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+print([x * x for x in range(1, 11) if x % 2 == 0]) # [4, 16, 36, 64, 100]
+print([m + n for m in 'ABC' for n in 'XYZ']) # ['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
+
+d = {'x': 'A', 'y': 'B', 'z': 'C' }
+print([k + '=' + v for k, v in d.items()]) # ['y=B', 'z=C', 'x=A']
+L = ['Hello', 'World', 5, 'IBM', 'Apple']
+print([s.lower() for s in L if isinstance(s, str)]) # ['hello', 'world', 'ibm', 'apple']
