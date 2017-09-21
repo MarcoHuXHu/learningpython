30e3c56861f7c3c369c8178aed0e2b3d09957b13
MarcoHuXHu <huxhu2333@gmail.com>
2017-09-21 22:08:07 +0800

    produce & permutations

--- Function/map&reduce.py
+++ Function/map&reduce.py
@@ -1,3 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
+# 0. 利用map和reduce来写一个str->int的函数
+def str2int(str):
+    def char2num(chr):
+        dic = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+        return dic[chr]
+
+    def nums2int(x, y):
+        return x * 10 + y
+
+    return reduce(nums2int, map(char2num, str))
+
+
+print(str2int('123'))
+
+# product, permutations
+
+# 1. A x B
+from itertools import product
+import re
+input_1a = '1, 2'
+input_1b = '2, 3'
+a = map(int, re.split(r'[\s,;]+', input_1a))
+b = map(int, re.split(r'[\s,;]+', input_1b))
+
+for i in product(a, b):
+    print(i, end=" ")
+print()
+
+# 2. 排列
+input2 = 'HACK 2'
+
+from itertools import permutations
+def join(a, b):
+    return a + b
+
+c, n = input2.split(" ")
+n = int(n)
+for i in sorted(permutations(c, n)):
+    print(reduce(join, i))
+
