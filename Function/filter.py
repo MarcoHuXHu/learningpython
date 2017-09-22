206923e78a2689968128aa68fcf75e8b472b4ab3
MarcoHuXHu <huxhu2333@gmail.com>
2017-09-22 16:05:43 +0800

    prime number

--- Function/filter.py
+++ Function/filter.py
@@ -9,6 +9,7 @@ def is_odd(n):
 print(list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])))
 # 结果: [1, 5, 9, 15]
 
+
 # 例1：筛选回文数
 
 def is_palindrome(n):#0123456
@@ -21,6 +22,7 @@ def is_palindrome(n):#0123456
 
 print(list(filter(is_palindrome, range(100, 999))))
 
+
 # 例2：筛选素数
 # 方法1：直接筛选法
 
@@ -28,7 +30,7 @@ import math
 
 def is_not_divisible(n):
     def func(x):
-        return (x%n) > 0
+        return (x==n) or (x%n) > 0  # x==n 这个条件是为了方法1准备的，以免把素数本身筛选掉了，方法2中不需要这个
     return func
 
 def prime_number(n):
@@ -40,3 +42,27 @@ def prime_number(n):
 
 prime_number(100)
 
+
+# 方法2：素数生成器，可以产生无限序列
+
+# 先定义一个初始iterator，生成奇数（素数除了2都是奇数）
+def init_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+# 素数生成器
+def prime_iter():
+    it = init_iter()
+    yield 2
+    while True:
+        n = next(it)
+        yield n
+        it = filter(is_not_divisible(n), it)
+
+for i in prime_iter():
+    if (i < 100):
+        print(i, end=", ")
+    else:
+        break
