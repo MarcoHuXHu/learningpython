ca0de9d1b22a26f7c0882ceafa41c5f2a7047b58
MarcoHuXHu <huxhu2333@gmail.com>
2017-09-26 21:31:11 +0800

    update urllib

--- WebCrawler/use-urllib.py
+++ WebCrawler/use-urllib.py
@@ -28,6 +28,9 @@ def url_example2():
     # req = request.Request('https://www.baidu.com', headers=header)
     # 与下面的方法是等价的
     req = request.Request('https://www.baidu.com')
+    # 如果要使用'PUT'或者'DELETE'方法
+    #request.get_method = lambda: 'PUT'
+    #request.get_method = lambda: 'DELETE'
     req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
     with request.urlopen(req) as response:
         data = response.read()
@@ -59,13 +62,14 @@ def url_example3():
     req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
     req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
 
-    with request.urlopen(req, data=login_data.encode('utf-8')) as f:
-        print('Status:', f.status, f.reason)
-        for k, v in f.getheaders():
-            print('%s: %s' % (k, v))
-        print('Data:', f.read().decode('utf-8'))
-
-
-
-
-url_example2()
+    # timeout的设置，设置等待多久超时，为了解决一些网站实在响应过慢而造成的影响
+    # 比如登录微博需要一些时间，设置timeout=1基本就超时了，这是会抛出异常
+    # with urlopen
+    try:
+        with request.urlopen(req, data=login_data.encode('utf-8'), timeout=1) as f:
+            print('Status:', f.status, f.reason)
+            for k, v in f.getheaders():
+                print('%s: %s' % (k, v))
+            print('Data:', f.read().decode('utf-8'))
+    except BaseException as e:
+        print(e) # <urlopen error _ssl.c:630: The handshake operation timed out>
