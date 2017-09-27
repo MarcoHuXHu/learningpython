86591566d281e28aa4ca0b3f88d1d0f8cfb3f780
MarcoHuXHu <huxhu2333@gmail.com>
2017-09-27 12:19:22 +0800

    beautiful soup

---
+++ WebCrawler/use-beautifulsoup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+from urllib import request
+
+def crawler_dongqiudi(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+    req = request.Request(url, headers=headers)
+    with request.urlopen(req) as response:
+        data = response.read()
+        with open('index.html', 'wb') as f:
+            f.write(data)
+
+    soup = BeautifulSoup(open('index.html'))
+
+    print(soup.title)
+
+
+crawler_dongqiudi('https://www.dongqiudi.com/team/50000515.html')
