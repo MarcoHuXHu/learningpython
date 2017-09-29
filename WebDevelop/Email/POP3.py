24c878e71cdffd2a20ca922e73ec5b7f5d2ac22e
MarcoHuXHu <huxhu2333@gmail.com>
2017-09-29 20:28:08 +0800

    Parse Email
    163 cannot log in again… stop here

--- WebDevelop/Email/POP3.py
+++ WebDevelop/Email/POP3.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import poplib
+from email.parser import Parser
+from email.header import decode_header
+from email.utils import parseaddr
 
 # 收件箱信息
 email_address = 'husidioi@163.com'
@@ -36,12 +39,39 @@ def fetch_emails():
     # 可以获得整个邮件的原始文本:
     msg_content = b'\r\n'.join(lines).decode('utf-8')
 
+    # 把邮件内容解析为Message对象：
+    # Message对象本身可能是一个MIMEMultipart对象，包含可能不止一层的嵌套的其他MIMEBase对象
+    msg = Parser().parsestr(msg_content)
+    print_email(msg)
+
     # 可以根据邮件索引号直接从服务器删除邮件:
     # server.dele(index)
     # 测试有效，并收到了提醒
     # stat()返回邮件数量和占用空间:
 
     server.quit()
+
+
+# 邮件的Subject或者Email中包含的名字都是经过编码后的str，要正常显示，就必须decode：
+def decode_str(s):
+    print(decode_header(s))
+    value, charset = decode_header(s)
+    if charset:
+        value = value.decode(charset)
+    return value
+
+
+# 将邮件嵌套的各层缩进展开：
+def print_email(msg, indent=0):
+    # 第0层，即From，To，Subject
+    if indent == 0:
+        for header in ['From', 'To', 'Subject']:
+            value = msg.get(header)
+            if value:
+                print(value)
+                print(decode_str(value))
+
+
 
 
 fetch_emails()
