a0eaa90d05f1aae649ced22d326a688b316b7e10
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-02 20:08:09 +0800

    1 to many and many to 1

--- Database/use-sqlalchemy.py
+++ Database/use-sqlalchemy.py
@@ -44,3 +44,25 @@ food.fat = 0.1
 session.commit()
 
 session.close()
+
+
+# 一对多和多对一
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+    # 一对多:
+    books = relationship('Book')
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+    # “多”的一方的book表是通过外键关联到user表的:
+    user_id = Column(String(20), ForeignKey('user.id'))
