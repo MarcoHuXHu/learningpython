51a3957c710b7cd2e0ea8eca00fcacfd07937b77
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-04 12:33:17 +0800

    simple-flask

--- WebDevelop/WebServer/simple-flask.py
+++ WebDevelop/WebServer/simple-flask.py
@@ -4,11 +4,35 @@
 from flask import Flask
 from flask import request
 
+# 这里的'__name__'是指Flask在搜索的module，
+# 对于single module的简单应用，使用'__name__'就OK了
+# 对于大的package应用，则设置为package所在目录名
 app = Flask(__name__)
 
-
-@app.route('/')
+# home page
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return '<h1>Hello World</h1>'
+    return '''<h1>Welcome</h1>
+                <p><button type="button" onclick="location='/login'">Log In</button></p>'''
 
-app.run(host='127.0.0.1', port=8888)
+# log in page
+@app.route('/login', methods=['GET'])
+def login():
+    # 返回一个login的form，用于输入用户名密码来登录
+    # 然后将form的内容post到对应到authenticate页
+    return '''<form action="/authenticate" method="post">
+                 <p><input name="username"></p>
+                 <p><input name="password" type="password"></p>
+                 <p><button type="submit">Log In</button></p>
+                 </form>'''
+
+# authenticate
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    # 需要从request对象读取表单内容：
+    if request.form['username'] == 'World' and request.form['password'] == 'password':
+        return '<h3>Hello, World!</h3>'
+    return '<h3>Wrong username or password.</h3>'
+
+app.run(host='127.0.0.1', port=9999)
+
--- WebDevelop/WebServer/wsgi-server.py
+++ WebDevelop/WebServer/wsgi-server.py
@@ -5,8 +5,9 @@
 # 一个简单的http server
 from wsgiref.simple_server import make_server
 # 导入自定义的application函数:
+# 注：import from自定义的.py时，需要把改目录marked as source root，这样Pycharm就不会标红
 from wsgiApp import application
 
-httpserver = make_server('', 8000, application)
+httpserver = make_server('127.0.0.1', 8000, application)
 print('Serving HTTP on port 8000...')
 httpserver.serve_forever()
