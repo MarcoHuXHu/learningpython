06d3d8cc0e613da4d9020767be079b2e5a431baf
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-04 18:32:41 +0800

    flask-template

--- WebDevelop/WebServer/use-flask-template.py
+++ WebDevelop/WebServer/use-flask-template.py
@@ -3,19 +3,28 @@
 
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='flask-templates')
 
 # home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    pass
+    return render_template('home.html')
 
 # log in page
 @app.route('/login', methods=['GET'])
 def login():
-    pass
+    return render_template('login.html')
 
 # authenticate
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
-    pass
+    # 需要从request对象读取表单内容：
+    if request.form['username'] == 'World' and request.form['password'] == 'password':
+        return render_template('welcome.html', username= request.form['username'])
+    else:
+        # 这里加username是为了登录失败form还保存username
+        # 另外html里面只有{{message}}和{{username}}，所以这里的参数这能设置这俩，对password无效
+        return render_template('login.html',  message='Bad username or password', username= request.form['username'])
+
+
+app.run(host='127.0.0.1', port=9998)
