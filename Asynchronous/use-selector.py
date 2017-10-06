eb6edcdf4c9ec7300f5f8b5e9b1afc56c5b8523e
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-06 23:10:58 +0800

    update selector

--- Asynchronous/use-selector.py
+++ Asynchronous/use-selector.py
@@ -3,9 +3,10 @@
 
 import socket
 from selectors import DefaultSelector, EVENT_READ, EVENT_WRITE
+import time
 
+start = time.time()
 selector = DefaultSelector()
-stopped = False
 urls = ['www.google.com', 'www.sina.com', 'www.baidu.com', 'www.sohu.com']
 
 class Crawler(object):
@@ -29,23 +30,21 @@ class Crawler(object):
         selector.unregister(key.fd)
         get = 'GET / HTTP/1.1\r\nHost: {0}\r\nConnection: close\r\n\r\n'.format(self.url)
         self.sock.send(get.encode('ascii'))
+        print("Connected and send request to %s at %s" % (self.url, time.time()-start))
         selector.register(key.fd, EVENT_READ, self.read_response)
 
     def read_response(self, key, mask):
-        chunk = self.sock.recv(4096)
+        chunk = self.sock.recv(409600)
         if chunk:
             self.response += chunk
         else:
-            print('Done with %s' % self.url)
+            print('Done with %s at %s' % (self.url, time.time()-start))
             selector.unregister(key.fd)
             urls.remove(self.url)
-            if not urls:
-                global stopped
-                stopped = True
 
 # Event Loop
 def loop():
-    while not stopped:
+    while urls:
         # 阻塞直到有事件发生
         events = selector.select()
         for event_key, event_mask in events:
@@ -58,6 +57,6 @@ if __name__ == '__main__':
     for url in urls:
         crawler = Crawler(url)
         crawler.fetch()
-    print(time.time()-start)
+    print(time.time() - start)
     loop()
     print(time.time()-start)
