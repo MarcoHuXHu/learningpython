bcf14b4a386c8e28b2da7714f2e5a7ffa81d7389
MarcoHuXHu <huxhu2333@gmail.com>
2017-10-17 23:40:41 +0800

    decorator functors.wraps

--- Function/decorator.py
+++ Function/decorator.py
@@ -1,3 +1,5 @@
+import functools
+
 # decorator without argument
 def log(func):
     def wrapper(*args, **kw):
@@ -8,16 +10,50 @@ def log(func):
 # decorator with argument
 def info(text):
     def decorator(func):
+        # 使用装饰器修饰过的函数func（下面例子的f3），实际上是一个decorator函数返回的“闭包”对象wrapper
+        # 所以要用functools模块的wraps装饰器，更正函数名
+        # 不然函数名，即__name__为wrapper
+        @functools.wraps(func)
         def wrapper(*args, **kw):
             print('%s %s():' % (text, func.__name__))
             return func(*args, **kw)
+
         return wrapper
     return decorator
 
+##### decorator without argument
 @log
 def f1():
-    print('2015-3-25')
+    print('f1')
 
-@info('execute hahaha')
 def f2():
-    print('2015-3-25')
+    print('f2')
+
+# 使用decorator
+f1()
+# 不使用decorator的等效用法
+f2 = log(f2)
+f2()
+
+
+##### decorator with argument
+@info('execute f3')
+def f3():
+    print('f3')
+
+def f4():
+    print('f4')
+
+# 使用decorator
+f3()
+# 不使用decorator的等效用法
+temp = info('execute f4')
+f4 = temp(f4)
+f4()
+
+
+###### @functools.wraps(func)的作用
+print(f1.__name__) # wrapper
+print(f2.__name__) # wrapper
+print(f3.__name__) # f3
+print(f4.__name__) # f4
